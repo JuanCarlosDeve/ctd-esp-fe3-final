fb25827aa015856e0e602be86de448376e9dda77
Juan Carlos <juancarlosvigar@gmail.com>
2024-09-13 16:43:19 -0500

    creacion de enrutados de las pag

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-
+import { Routes, Route } from "react-router-dom"
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home"
+import Detail from "./Routes/Detail"
+import Favs from "./Routes/Favs";
+import Contact from "./Routes/Contact";
+import { routes } from "./Components/utils/routers"
+
 
 
 function App() {
   return (
+    <>
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        <Navbar />
+        <Routes>
+
+          <Route path={routes.home} element={<Home />} />
+          <Route path={routes.contact} element={<Contact />} />
+          <Route path="detail/:id" element={<Detail />} />
+          <Route path={routes.favs} element={<Favs />} />
+          <Route path={routes.notFound} element={<h1>PAGE NOT FOUND</h1>} />
+        </Routes>
+
+        <Footer />
       </div>
+    </>
+
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { routes } from './utils/routers'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -8,6 +10,10 @@ const Navbar = () => {
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <Link to={routes.home}>Home</Link>
+      <Link to={routes.contact}>Contacto</Link>
+      <Link to={routes.favs}>Favoritos</Link>
+
       <button>Change theme</button>
     </nav>
   )
